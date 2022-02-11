5fde85593e564fe83b1105aa8e9a0e2ba7503d49
Marcin <mistamasta7@gmail.com>
2022-02-11 17:59:27 +0100

    Add Feedback component & changes for settings.scss

---
+++ src/components/layout/Feedback/Feedback.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import styles from './Feedback.module.scss';
+import { faQuoteRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import PropTypes from 'prop-types';
+
+class Feedback extends React.Component {
+  state = {
+    activePage: 0,
+  };
+
+  render() {
+    const { opinions } = this.props;
+    const { activePage } = this.state;
+    const pagesAmount = Math.ceil(opinions.length);
+
+    const points = [];
+    for (let v = 0; v < pagesAmount; v++) {
+      points.push(
+        <li key={v}>
+          <a className={v === activePage ? styles.active : ''}>page {v}</a>
+        </li>
+      );
+    }
+
+    return (
+      <div className={styles.root}>
+        <div className='container'>
+          <div className={styles.feedbackMainBar}>
+            <div className='row no-gutters align-items-end'>
+              <div className={'col-auto ' + styles.heading}>
+                <h3>Customer Feedback</h3>
+              </div>
+              <div className='col'></div>
+              <div className={'col-auto ' + styles.points}>
+                <ul>{points}</ul>
+              </div>
+            </div>
+            <div className={styles.icon}>
+              <FontAwesomeIcon icon={faQuoteRight} />
+            </div>
+          </div>
+          <div className={styles.opinionArea}>
+            {opinions.slice(activePage, activePage + 1).map(feed => (
+              <div key={feed.id} className='col text-center'>
+                <div className='row justify-content-md-center'>
+                  <p className={styles.opinionCont}>{feed.customerOpinion}</p>
+                </div>
+                <div className='row justify-content-md-center'>
+                  <div className='col col-lg-1'>
+                    <img
+                      className={styles.customerPhoto}
+                      src={feed.customerPhoto}
+                      alt='Avatar'
+                    />
+                  </div>
+                  <div className={'col col-lg-2 ' + styles.spec}>
+                    <h6>{feed.customerName}</h6>
+                    <p className={styles.customerState}>Furniture Customer</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Feedback.propTypes = {
+  children: PropTypes.node,
+  opinions: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      customerName: PropTypes.string,
+      customerPhoto: PropTypes.string,
+      customerOpinion: PropTypes.string,
+    })
+  ),
+};
+
+Feedback.defaultProps = {
+  opinions: [],
+};
+
+export default Feedback;
