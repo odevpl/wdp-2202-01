851ef276ec498a430881bab46bd7b6b7dc1420ae
Marcin <mistamasta7@gmail.com>
2022-02-16 23:56:00 +0100

    Resolve conflicts

--- src/components/features/NewFurniture/NewFurniture.js
+++ src/components/features/NewFurniture/NewFurniture.js
@@ -1,34 +1,56 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import styles from './NewFurniture.module.scss';
 import ProductBox from '../../common/ProductBox/ProductBox';
-import Swipeable from '../../common/Swipeable/Swipeable';
 
+import { connect } from 'react-redux';
+
+import PropTypes from 'prop-types';
+import React from 'react';
+import styles from './NewFurniture.module.scss';
+
+import Swipeable from '../../common/Swipeable/Swipeable';
 class NewFurniture extends React.Component {
   state = {
     activePage: 0,
     activeCategory: 'bed',
-    activePageEffect: styles.fadeIn,
+    isFading: false,
   };
 
   handlePageChange(newPage) {
-    this.setState({ activePageEffect: styles.fadeOut });
-    setTimeout(() => {
-      this.setState({ activePage: newPage, activePageEffect: styles.fadeIn });
-    }, 400);
+    this.setState({
+      activePage: newPage,
+      isFading: true,
+    });
+    if (this.state.isFading === false) {
+      setTimeout(
+        function() {
+          this.setState({
+            isFading: false,
+          });
+        }.bind(this),
+        900
+      );
+    }
   }
 
   handleCategoryChange(newCategory) {
-    this.setState({ activePageEffect: styles.fadeOut });
-    setTimeout(() => {
-      this.setState({ activeCategory: newCategory, activePageEffect: styles.fadeIn });
-    }, 400);
+    this.setState({
+      activeCategory: newCategory,
+      isFading: true,
+    });
+    if (this.state.isFading === false) {
+      setTimeout(
+        function() {
+          this.setState({
+            isFading: false,
+          });
+        }.bind(this),
+        900
+      );
+    }
   }
 
   render() {
     const { categories, products } = this.props;
-    const { activeCategory, activePage, activePageEffect } = this.state;
+    const { activeCategory, activePage, isFading } = this.state;
 
     const categoryProducts = products.filter(item => item.category === activeCategory);
     const pagesCount = Math.ceil(categoryProducts.length / 8);
@@ -38,111 +60,92 @@ class NewFurniture extends React.Component {
       dots.push(
         <li key={i}>
           <a
-            href='/#'
-            onClick={() => {
-              this.handlePageChange(i);
-            }}
-            className={i === activePage ? styles.active : ''}
+            onClick={() => this.handlePageChange(i)}
+            className={i === activePage ? styles.active : undefined}
           >
-            page {i}
-          </a>
+            page {i}{' '}
+          </a>{' '}
         </li>
       );
     }
+
     const pages = [];
     for (let i = 0; i < pagesCount; i++) {
       pages.push(
         categoryProducts
           .slice(activePage * 8, (activePage + 1) * 8)
           .map((item, index) => {
-            const dummy = this.props.favorites.find(product => product.id === item.id)
+            const favorite = this.props.favorites.find(
+              product => product.id === item.id
+            )
               ? true
               : '';
+            const addedForComparison = this.props.comparedProducts.find(
+              product => product.id === item.id
+            )
+              ? true
+              : '';
+
             return (
               <div key={item.id} className='col-3'>
                 <ProductBox
                   {...item}
                   product={item}
-                  favorite={dummy}
-                  page={activePage}
-                  index={index}
-                />
+                  favorite={favorite}
+                  addedForComparison={addedForComparison}
+                />{' '}
               </div>
             );
           })
       );
     }
+
     return (
       <div className={styles.root}>
         <div className='container'>
           <div className={styles.panelBar}>
             <div className='row no-gutters align-items-end'>
-              <div className={'col-12 col-md-auto ' + styles.heading}>
-                <h3>New furniture</h3>
-              </div>
-              <div className={'col-auto col-md ' + styles.menu}>
+              <div className={'col-auto ' + styles.heading}>
+                <h3> New furniture </h3>{' '}
+              </div>{' '}
+              <div className={'col ' + styles.menu}>
                 <ul>
+                  {' '}
                   {categories.map(item => (
                     <li key={item.id}>
                       <a
-                        href='/#'
-                        className={
-                          item.id === activeCategory ? styles.active : undefined
-                        }
-                        onClick={() => {
-                          this.handleCategoryChange(item.id);
-                        }}
+                        className={item.id === activeCategory && styles.active}
+                        onClick={() => this.handleCategoryChange(item.id)}
                       >
-                        {item.name}
-                      </a>
+                        {item.name}{' '}
+                      </a>{' '}
                     </li>
-                  ))}
-                </ul>
-              </div>
+                  ))}{' '}
+                </ul>{' '}
+              </div>{' '}
               <div className={'col-auto ' + styles.dots}>
-                <ul>{dots}</ul>
-              </div>
-            </div>
-          </div>
-          <div className={'row ' + activePageEffect}>
-          <Swipeable
+                <ul> {dots} </ul>{' '}
+              </div>{' '}
+            </div>{' '}
+          </div>{' '}
+          <div className={`row ${isFading ? styles.fadeIn : styles.fadeOut}`}>
+            <Swipeable
               activePage={activePage}
               handlePageChange={this.handlePageChange.bind(this)}
               pages={pages}
-            />
-            ;
-            {categoryProducts.slice(activePage * 8, (activePage + 1) * 8).map((item, index) => {
-              const favorite = this.props.favorites.find(
-                product =>product.id === item.id
-              )
-                ? true
-                : '';
-              const addedForComparison = this.props.comparedProducts.find(
-                product => product.id === item.id
-              )
-                ? true
-                : '';
-              return (
-              <div key={item.id} className={`col-6 col-md-4 col-lg-3}`}>
-                <ProductBox
-                {...item}
-                product={item}
-                favorite={favorite}
-                addedForComparison={addedForComparison}
-                />
-              </div>
-            );
-          })}
-          </div>
-        </div>
+            />{' '}
+          </div>{' '}
+        </div>{' '}
       </div>
     );
   }
 }
+
 NewFurniture.propTypes = {
   children: PropTypes.node,
   favorites: PropTypes.array,
   comparedProducts: PropTypes.array,
+
   categories: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string,
@@ -167,4 +170,9 @@ NewFurniture.defaultProps = {
   products: [],
 };
 
-export default NewFurniture;
+const mapStateToProps = state => ({
+  favorites: state.favorites,
+  comparedProducts: state.comparedProducts,
+});
+
+export default connect(mapStateToProps)(NewFurniture);
