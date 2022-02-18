3298d0ccde9158e4e584561794eb71387efe10fc
Krystian Matkowski <ScoverStudio@gmail.com>
2022-02-18 12:13:48 +0100

    conflicts resolved

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -54,6 +54,9 @@ const ProductBox = ({ name, price, promo, ...props }) => {
         </div>
       </div>
       <div className={styles.content}>
+        <h5>{name}</h5>
+        <StarRating product={props.product} />
+
         <NavLink to={`/product/${props.id}`}>
           <h5>{name}</h5>
         </NavLink>
