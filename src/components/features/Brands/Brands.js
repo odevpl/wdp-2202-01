ad9b103366adaea8b2b8ccbfe4d0c194eef383d1
Marta Nara <marta.kos1230@gmail.com>
2022-02-22 14:51:00 +0100

    Add styles for slider

--- src/components/features/Brands/Brands.js
+++ src/components/features/Brands/Brands.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Brands.module.scss';
 import Slider from 'react-slick';
+import './BrandsSlider.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
@@ -10,27 +11,60 @@ class Brands extends React.Component {
     const { brands } = this.props;
 
     const settings = {
-      dots: true,
+      dots: false,
       infinite: true,
       speed: 500,
-      slidesToShow: 3,
-      slidesToScroll: 3,
+      slidesToShow: 6,
+      slidesToScroll: 6,
+      adaptiveHeight: true,
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 5,
+          },
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          },
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          },
+        },
+        {
+          breakpoint: 500,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          },
+        },
+      ],
     };
 
     return (
       <div className={styles.root}>
         <div className='container'>
-          <div className={styles.brandsBar}>
-            <Slider {...settings}>
-              {brands.map(brand => {
-                return (
-                  <div className={styles.brand} key={brand.id.replace('brand-', '')}>
-                    <img src={`${brand.image}`} alt='brand' />
-                  </div>
-                );
-              })}
-            </Slider>
-          </div>
+          <Slider {...settings}>
+            {brands.map(brand => {
+              return (
+                <div key={brand.id.replace('brand-', '')} className={styles.sliderDiv}>
+                  <img
+                    key={brand.id.replace('brand-', '')}
+                    src={`${process.env.PUBLIC_URL}/images/brands/${brand.id}.jpeg`}
+                    alt='brand'
+                  />
+                </div>
+              );
+            })}
+          </Slider>
         </div>
       </div>
     );
