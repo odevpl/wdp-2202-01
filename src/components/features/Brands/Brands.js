a6ca5ee75f393bf5232b267cccd54164cc0b707a
Marta Nara <marta.kos1230@gmail.com>
2022-02-22 15:33:24 +0100

    Delete unused code

--- src/components/features/Brands/Brands.js
+++ src/components/features/Brands/Brands.js
@@ -76,35 +76,3 @@ Brands.propTypes = {
 };
 
 export default Brands;
-
-/*
-
-return (
-      <div className={styles.root}>
-        <div className='container'>
-          <div className={'row align-items-center ' + styles.brandsBar}>
-            <Button className={styles.button}>
-              <span className={styles.sign}>&#x2329;</span>
-            </Button>
-            <ul className={styles.brandsList}>
-              {brands.map(brand => {
-                return (
-                  <li
-                    className={styles.brand}
-                    key={brand.id.replace('brand-', '')}
-                    style={{ backgroundImage: `url('${brand.image}')` }}
-                    alt='brand'
-                  ></li>
-                );
-              })}
-            </ul>
-            <Button className={styles.button}>
-              <span className={styles.sign}>&#x232A;</span>
-            </Button>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  */
